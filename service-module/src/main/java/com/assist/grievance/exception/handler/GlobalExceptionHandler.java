27a23554d5b975fc9f180981ecf91549bba5b0bd
Shishupal Lohani <shishupallohani70@gmail.com>
2026-03-10 01:49:57 +0530

    Handle missing static resources and add apple touch icons

--- service-module/src/main/java/com/assist/grievance/exception/handler/GlobalExceptionHandler.java
+++ service-module/src/main/java/com/assist/grievance/exception/handler/GlobalExceptionHandler.java
@@ -6,9 +6,11 @@ import com.assist.grievance.commonUtil.ResponseData;
 import com.assist.grievance.exception.BusinessException;
 import com.assist.grievance.exception.ExportException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 import java.util.Collections;
 
@@ -33,6 +35,17 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ResponseData> handleBusinessException(BusinessException ex) {
         log.warn("BusinessException caught: {}", ex.getMessage());
         return CommonUtil.generateCustomResponse(ex.getMessage(), Constants.ERROR, ex.getHttpStatus());
+    }
+
+    @ExceptionHandler(NoResourceFoundException.class)
+    public ResponseEntity<ResponseData> handleNoResourceFoundException(NoResourceFoundException ex) {
+        String resourcePath = ex.getResourcePath();
+        if ("apple-touch-icon.png".equals(resourcePath) || "apple-touch-icon-precomposed.png".equals(resourcePath)) {
+            log.debug("Optional browser icon not found: {}", resourcePath);
+        } else {
+            log.warn("Static resource not found: {}", resourcePath);
+        }
+        return CommonUtil.generateCustomResponse("Resource not found", Constants.ERROR, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(Exception.class)
