8ba3df84dff2ddab5929afd0ebf48edc6b5a795b
Shishupal Lohani <shishupallohani70@gmail.com>
2026-07-10 15:15:45 +0530

    pom

--- service-module/src/main/java/com/assist/grievance/producer/GrievanceKafkaProducer.java
+++ service-module/src/main/java/com/assist/grievance/producer/GrievanceKafkaProducer.java
@@ -6,9 +6,9 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 
-@Service
+@Component
 @RequiredArgsConstructor
 @Slf4j
 public class GrievanceKafkaProducer {
